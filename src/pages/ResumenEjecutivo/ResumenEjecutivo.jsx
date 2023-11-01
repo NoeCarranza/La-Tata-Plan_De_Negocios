2dafbd87d8b609c56ac7cd2c592fd2352c6c0d2f
Noe <noe91.carranza@gmail.com>
2023-11-01 19:04:05 -0300

    responsive plan de marketing

--- src/pages/ResumenEjecutivo/ResumenEjecutivo.jsx
+++ src/pages/ResumenEjecutivo/ResumenEjecutivo.jsx
@@ -19,10 +19,10 @@ export default function ResumenEjecutivo() {
             </div>
             <div className='parte2_RE'>
                 <img src={img1} alt="babysitter" className='img-tata'></img>
-                <p className='texto-parte2'>Las actividades centrales de La Tata incluyen una cuidadosa selección y capacitación de cuidadores calificados, el desarrollo y mantenimiento de una aplicación móvil y una página web, la atención al cliente personalizada y la coordinación eficiente de servicios, estrategias de promoción y marketing, así como una gestión financiera y administrativa eficiente. Además, la empresa se compromete con un monitoreo continuo de la calidad del servicio y planea expandirse a nuevas áreas geográficas en el futuro.</p>
+                <p className='texto-parte2'>Las actividades centrales de La Tata incluyen una cuidadosa selección y capacitación de cuidadores calificados, el desarrollo y mantenimiento de una aplicación móvil y una página web, la atención al cliente personalizada y la coordinación eficiente de servicios, estrategias de promoción y marketing, así como una gestión financiera y administrativa eficiente.<br/><br/>Además, la empresa se compromete con un monitoreo continuo de la calidad del servicio y planea expandirse a nuevas áreas geográficas en el futuro.</p>
             </div>
             <div className='parte3_RE'>
-                <p className='texto-parte3'>Para garantizar un servicio de alta calidad, La Tata implementa programas de capacitación continua y supervisión regular. La empresa también recopila la retroalimentación de los clientes para mejorar y ajustar los procesos de atención y cuidado. Además, establece protocolos estrictos de seguridad y protección para garantizar la integridad física y emocional de los niños a su cargo.</p>
+                <p className='texto-parte3'>Para garantizar un servicio de alta calidad, La Tata implementa programas de capacitación continua y supervisión regular. <br/><br/>La empresa también recopila la retroalimentación de los clientes para mejorar y ajustar los procesos de atención y cuidado. <br/><br/>Además, establece protocolos estrictos de seguridad y protección para garantizar la integridad física y emocional de los niños a su cargo.</p>
                 <img src={img2} alt="babysitter" className='img-tata'></img>
             </div>
         </section>
